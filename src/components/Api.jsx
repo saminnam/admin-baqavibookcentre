43c52cfbd47ab7e5a85a11fecd59d92197203359
Your Name <developerinnam111@gmail.com>
2026-06-08 18:20:07 +0530

    api-change

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://e-commerce-app-backend-livid.vercel.app/apihttps://e-commerce-app-backend1.vercel.app/api"
+export const API_BASE_URL = "https://e-commerce-app-backend1.vercel.app/api";
