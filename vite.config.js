c084d151545c7eb3038d77276031eab3d947addd
saminnam <developerinnam111@gmail.com>
2026-06-12 23:05:14 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  //   server: {
+  //   port: 5174,
+  // },
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [react(), tailwindcss()],
 });
